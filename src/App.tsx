4f3d7b612f7acef4c3b4bf24bf5f5e6f4e07e338
Ashish <ashishsaud498@gmail.com>
2025-01-30 05:30:09 +0545

    debugging...

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { loadConfigFromFile } from "vite";
 
 interface dateType {
   month: string;
@@ -13,9 +12,8 @@ const App = () => {
     year: 0,
   });
 
-  const [city, setCity] = useState<string | undefined>("kathmandu");
+  const [city, setCity] = useState<string | undefined>("");
   const [weatherData, setWeatherData] = useState<any>(null);
-
 
   const apiKey: string = "9f3a23e1579de3196e33818d8f1c3d9f";
 
@@ -47,10 +45,13 @@ const App = () => {
   };
 
   useEffect(() => {
-    fetchWeatherData();
+    city && fetchWeatherData();
   }, [city]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === "") {
+      setWeatherData(null);
+    }
     setCity(e.target.value);
     console.log(e.target.value);
   };
@@ -59,60 +60,76 @@ const App = () => {
     fetchWeatherData();
   };
 
-  const getWeatherIconUrl = (weather: string):string => {
+  const getWeatherIconUrl = (weather: string): string => {
     switch (weather) {
       case "Clouds":
-        return "/Clouds.png"; 
+        return "/Clouds.png";
       case "Rain":
-        return "/rain_with_cloud.png"; 
+        return "/rain_with_cloud.png";
       case "Mist":
-        return "/Mist.png"; 
-      case "Haze":  
-        return "/sun.png"; 
-        case "Clear" : 
+        return "/Mist.png";
+      case "Haze":
+        return "/sun.png";
+      case "Clear":
         return "/Clear.png";
+      case "Snow":
+        return "/Snow.png";
       default:
         return "./default.png";
     }
-  }
+  };
 
   return (
     <>
       <div className="text-center">
         <div className="bg-black bg-gradient-to-tr from-cyan-500 to-blue-500  w-full h-[600px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center rounded-lg sm:w-[550px] sm:h-[650px]">
-          <h1 className="text-white mt-20 opacity-50 text-xl font-normal">
+
+          <h1 className="text-white mt-20 opacity-70 text-xl font-normal">
             {currentDate.month} {currentDate.day}, {currentDate.year}
           </h1>
-          {weatherData.name && (
+          {weatherData ? (
             <div>
-              <h2 className="mt-4 text-4xl font-bold text-white">{weatherData.name}</h2>
-
-              
+              <h2 className="mt-4 text-4xl font-bold text-white">
+              {weatherData?.name || "unknown city ..."}
+              </h2>
 
               <img
                 className="mt-4 mx-auto"
-                src={getWeatherIconUrl(weatherData.weather[0].main)}
+                src={getWeatherIconUrl(weatherData?.weather?.[0]?.main)}
                 alt="weather picture"
                 width={180}
               />
 
-              <h2 className="text-5xl font-extrabold text-white ">{weatherData.main.temp}°C</h2>
+              <h2 className="text-5xl font-extrabold text-white ">
+                {weatherData?.main?.temp || "-"}°C
+              </h2>
 
-              <h2 className=" text-white text-lg">{weatherData.weather[0].main}, feels like {weatherData.main.
-feels_like}°C</h2>
+              <h2 className=" text-white text-lg">
+                {weatherData?.weather?.[0]?.main || "---"}, feels like  {weatherData?.main?.feels_like || "-"}°C
+              </h2>
+            </div>
+          ) :
+          (
+            <div>
+              <h1 className="text-4xl sm:text-5xl text-white font-bold my-5 ">
+                Weather App
+              </h1>
+          
+              <img
+                className="my-4 mx-auto"
+                width={180}
+                src="./rain_with_cloud.png"
+                alt="weather picture"
+              />
+              <h1 className="text-4xl mt-8 text-white opacity-80">
+                Please enter the city name
+              </h1>
             </div>
           )}
 
-{/* (
-            <div>
-              <h1 className="text-4xl sm:text-5xl text-white font-bold my-5 ">Weather App</h1>
 
+          
 
-              <img className="my-4 mx-auto" width={180}
-               src="./rain_with_cloud.png" alt="weather picture" />
-              <h1 className="text-4xl mt-8 text-white opacity-80">Please enter the city name</h1>
-            </div>
-          ) */}
 
           <form
             onSubmit={handleSubmit}
